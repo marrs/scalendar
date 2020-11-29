667af6ccb42e586c9bd0f0b93ccf85dfd7557079
David Marrs <david@marrsbytes.com>
2020-11-29 17:41:48 +0000

    Choose which calendar file to use

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,111 @@ use std::env;
 use std::io::{self, Write};
 use regex::Regex;
 
+type Days = u32;
+
 struct Args {
-    days_to_come: u32,
-    days_past: u32,
+    // TODO: Implement error message
+    days_to_come: Days,
+    days_past: Days,
+    calendar_file: Option<String>
 }
+
+impl Args {
+    fn to_vec(&self, flags: &[&str]) -> Vec<String> {
+        let mut result = vec!();
+        for x in flags.iter() {
+            match x {
+                &"-f" => {
+                    match &self.calendar_file {
+                        Some(xx) => {
+                            result.push("-f".to_string());
+                            result.push(xx.to_string());
+                        },
+                        None     => ()
+                    }
+                },
+                _ => () // TODO: Log error
+
+            }
+        }
+        return result;
+    }
+}
+
+enum LogDest {
+    None,
+    StdErr,
+}
+
+static mut LOG_DEST: LogDest = LogDest::None;
+
+/*
+fn log_info(x: &str) {
+    match LOG_DEST {
+        LogDest::None => (),
+        LogDest::StdErr => io::stderr().write_all(&x[..]).unwrap()
+    }
+}
+
+fn log_error(x: &str) {
+    match LOG_DEST {
+        LogDest::None => (),
+        LogDest::StdErr => io::stderr().write_all(x).unwrap()
+    }
+}
+*/
 
 fn parse_args(args: env::Args) -> Args {
     let pos_int_regex = Regex::new(r"^\d+$").unwrap();
     let mut found_flag = "";
-    let mut parsed_args = Args { days_to_come: 1, days_past: 0 };
+    let mut days_to_come = 1;
+    let mut days_past = 0;
+    let mut calendar_file = None;
     for arg in args {
         // TODO: Implement -B
         if found_flag == "-l" || found_flag == "-A" {
             if pos_int_regex.is_match(&arg) {
-                println!("Future days argument value found");
-                parsed_args.days_to_come = arg.to_string().parse::<u32>().unwrap();
+                //log_info("Future days argument value found");
+                days_to_come = arg.parse::<u32>().unwrap();
             } else {
-                println!("Future days argument value malformed");
+                //log_error("Future days argument value malformed");
             }
             found_flag = "";
+        } else if found_flag == "-f" {
+            calendar_file = Some(arg);
+            found_flag = "";
         } else if arg == "-l" || arg == "-A" {
-            println!("Future days argument found");
+            //log_info("Future days argument found");
             found_flag = "-l";
+        } else if arg == "-f" {
+            found_flag = "-f";
+        } else if arg == "-v" {
+            unsafe {
+                LOG_DEST = LogDest::StdErr
+            }
         }
     }
-    return parsed_args;
+    return Args {
+        days_to_come: days_to_come,
+        days_past: days_past,
+        calendar_file: calendar_file
+    };
 }
 
 fn main() {
-    println!("Hello, world!");
-    let cmd = Command::new("calendar").arg("-l365").output().expect("Failed to execute 'calendar -l365'");
-
     // TODO:
     //   * Show events for current range:
     //     * value passed to -l,
     //     * 1 by default.
     //   * Include events with reminder that falls within Future days.
+    let args = parse_args(env::args());
+    let cmd = Command::new("calendar").arg("-l365").args(args.to_vec(&["-f"])).output().expect("Failed to execute 'calendar -l365'");
+
     io::stdout().write_all(&cmd.stdout).unwrap();
     io::stderr().write_all(&cmd.stderr).unwrap();
 
-    let args = parse_args(env::args());
-    println!("days to come: {}", args.days_to_come);
-    println!("days past: {}", args.days_past);
+    //log_info(&format!("days to come: {}", args.days_to_come)[..]);
+    //log_info(&format!("days past: {}", args.days_past)[..]);
 
 /*
 assert!(output.status.success());
